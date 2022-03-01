b657ea109f5e0e3bbba6243bbb91a5007bcb36e5
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2022-03-01 10:26:05 -0300

    feat: pages added

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link";
+
+const FaqPage = () => {
+    return (
+        <div>
+            <h1>FAQ PAGE</h1>
+            <Link href="/">ir para a home</Link>
+        </div>
+    );
+};
+
+export default FaqPage;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link";
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Minha page inicial</h1>
+            <Link href="/faq">Ir para o faq</Link>
+        </div>
+    );
+};
+
+export default HomePage;
